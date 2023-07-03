74b7878d84c17db13c5961cc9dd9e3a8ab1a6622
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-03 09:23:28 -0500

    feat: patron DTO

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,15 +1,17 @@
 import UserService from "../services/userService.js"
+import UserDTO from "../dtos/userDto.js"
 const userService = new UserService()
 
 const getUsers = async(req, res) => {
     let result = await userService.getUsers()
-    res.send(result)
+    let resultDTO = result.map(user => new UserDTO(user))
+    res.send(resultDTO)
 }
 
 const saveUser = async(req, res) => {
     let user = req.body
     let result = await userService.addUser(user)
-    res.send(result)
+    res.send(new UserDTO(result))
 }
 
 export default { getUsers, saveUser }
